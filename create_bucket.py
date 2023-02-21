0196d09f9ce2c44f5ad8d4b29f89f5c9758436b5
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-02-21 18:03:05 +0000

    s3/ec3 updated

--- create_bucket.py
+++ create_bucket.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-import sys
 import boto3
+import random
+import string
+
+digits = random.choices(string.digits, k=4)
+letters = random.choices(string.ascii_lowercase, k= 5)  
 s3 = boto3.resource("s3")
-for bucket_name in sys.argv[1:]:
-    try:
-        response = s3.create_bucket(Bucket=bucket_name)
-        print (response)
-    except Exception as error:
-         print (error)
+
+name= random.sample(digits + letters, 9)
+
+bucket_name = "bucket" + ''.join(name)
+
+s3.create_bucket(Bucket=bucket_name)
+
+print("Unique bucket name: " + bucket_name)
