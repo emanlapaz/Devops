0c8ccba133faca0fb8fd1edc50ad695138488c75
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-02-21 18:03:16 +0000

    week 4 done

--- week4.py
+++ week4.py
@@ -1,6 +1,11 @@
 import boto3
 import webbrowser
 import time
+import sys
+import random
+import string
+
+#EC2 instance
 ec2 = boto3.resource('ec2')
 
 new_instances = ec2.create_instances(
@@ -38,10 +43,33 @@ def get_public_ip(instanceID):
 
 public_add = get_public_ip(instanceID)
 
+#S3 bucket:
+
+#randomly selects 4 lowercase letter and digits
+digits = random.choices(string.digits, k=4)
+letters = random.choices(string.ascii_lowercase, k= 4)  
+s3 = boto3.resource("s3")
+
+#shuffles the letters and digits
+name= random.sample(digits + letters, 8)
+
+bucket_name = "bucket" + ''.join(name)
+
+s3.create_bucket(Bucket=bucket_name)
+website_configuration = {
+    'ErrorDocument': {'Key': 'error.html'},
+    'IndexDocument': {'Suffix': 'index.html'},
+}
+
+bucket_website = s3.BucketWebsite(bucket_name)
+response = bucket_website.put(WebsiteConfiguration=website_configuration)
+
+
 #print to terminal
 print("Your instance is up and running")
-print ("New instance created:"+instanceID)
-print("Public Address:",public_add)
+print ("New instance created:"+instanceID, "Public Address:",public_add)
+print("Unique bucket name: " + bucket_name)
+print("Upload an index.html file to test it works!" )
 time.sleep(30)
 webbrowser.open_new_tab(public_add)
 print("Web Browser Open")
