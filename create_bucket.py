348a755605d74774a00e89520c7e7447296ed406
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-02-24 03:34:13 +0000

    updated create bucket file

--- create_bucket.py
+++ create_bucket.py
@@ -20,11 +20,11 @@ print("Unique bucket name: " + bucket_name)
 
 
 #url to download image
-url = "http://devops.witdemo.net/logo.jpg"
+url_src = "http://devops.witdemo.net/logo.jpg"
 #output path to images folder
 output_path = "/home/eugene/Public/image.jpg"
-urllib.request.urlretrieve(url, output_path)
-print("image downloaded from:", url)
+urllib.request.urlretrieve(url_src, output_path)
+print("image downloaded from:", url_src)
 
 #uploading image to bucket
 
@@ -35,32 +35,33 @@ def upload_to_bucket():
     s3 = boto3.client('s3')
 
     # set the bucket name and image file name
-    s3_buck = bucket_name
+    s3_bucket = bucket_name
     wit_logo = 'image.jpg'
     index = 'index.html'
     region = 'us-east-1'
 
     # upload the image to the bucket and make it public
-    s3.upload_file(wit_logo, s3_buck, wit_logo, ExtraArgs={'ContentType': 'image/jpeg'})
-    s3.put_object_acl(Bucket=s3_buck, Key=wit_logo , ACL='public-read')
+    s3.upload_file(wit_logo, s3_bucket, wit_logo, ExtraArgs={'ContentType': 'image/jpeg'})
+    s3.put_object_acl(Bucket=s3_bucket, Key=wit_logo , ACL='public-read')
     print("upload 1 done")
-    s3.upload_file(index, s3_buck, index, ExtraArgs={'ContentType': 'text/html'})
-    s3.put_object_acl(Bucket=s3_buck, Key=index , ACL='public-read')
+    s3.upload_file(index, s3_bucket, index, ExtraArgs={'ContentType': 'text/html'})
+    s3.put_object_acl(Bucket=s3_bucket, Key=index , ACL='public-read')
     print("upload 2 done")
 
+
     # open the image in the default web browser
-    url = f"http://{bucket_name}.s3-website-{region}.amazonaws.com/{wit_logo}"
-    webbrowser.open(url)
+    uploaded_url = f"http://{bucket_name}.s3-website-{region}.amazonaws.com"
+    webbrowser.open(uploaded_url)
+    #print the uploaded url
+    print("Amazon S3 URL for the uploaded image:", uploaded_url)
 
 
 website_configuration = {
     'ErrorDocument': {'Key': 'error.html'},
-    'IndexDocument': {'Suffix': 'index.html'},
+    'IndexDocument': {'Suffix':'index.html'},
 }
 
 bucket_website = s3.BucketWebsite(bucket_name)
 response = bucket_website.put(WebsiteConfiguration=website_configuration)
 
-
-amazon_url = upload_to_bucket()
-print("Amazon S3 URL for the uploaded image:", amazon_url)
+upload_to_bucket()
